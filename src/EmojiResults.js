7c548c49b6bb7e460d8bccae3ca5126dbf50befb
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-02 15:01:08 +0300

    Added data-testid

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -20,7 +20,7 @@ export default class EmojiResults extends PureComponent {
 
   render() {
     return (
-      <div className="component-emoji-results">
+      <div data-testid="emoji-list" className="component-emoji-results">
         {this.props.emojiData.map(emojiData => (
           <EmojiResultRow
             key={emojiData.title}
